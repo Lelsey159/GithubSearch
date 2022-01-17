e5238e19aa2efbdcd19d2e07e8de2aa919947b74
Lesley159 <lesleyhope159@gmail.com>
2022-01-17 03:53:13 +0300

    updated get user class

--- src/app/get-service/get.service.ts
+++ src/app/get-service/get.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetService {
+  username: string,
+  repoName: string, 
+  clientId: string = "";
+  clientSecret: string = "";
+  apikey:string='';
+  constructor(private http: HttpClient) { 
+    console.log('service is now ready');
+  }
+  getUserRepos() {
+    return this.http.get('https://api.github.com/users/' + this.username + '/repos'+ "?client_id=" + this.clientId + "&client_secret=" + this.clientSecret);
+  }
+  getUser() {
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientId + "&client_secret=" + this.clientSecret);
+  }
 
-  constructor() { }
+  searchrepos() {
+    return this.http.get('https://api.github.com/search/repositories?q=' + this.repoName, ({
+      headers: new HttpHeaders({Authorization: `token ${this.apikey}`})
+    }))
+  }
+
+  UpdateUser(username:string) {
+    this.username = username;
+  }
+
+  UpdateRepo(repo:string) {
+    this.repoName = repo;
+  }
+
 }
