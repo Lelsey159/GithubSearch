1779dc07f5a830317f5e79f01aa89a2bcee01471
Lesley159 <lesleyhope159@gmail.com>
2022-01-17 04:09:07 +0300

    function on how to find repo

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GetService } from '../get-service/get.service';
 @Component({
   selector: 'app-repositories',
   templateUrl: './repositories.component.html',
   styleUrls: ['./repositories.component.css']
 })
 export class RepositoriesComponent implements OnInit {
+  repoitems: any[];
+  repoName:string= "bootsrap";
 
-  constructor() { }
+  constructor(private GetService: GetService) { 
+    
+  }
 
-  ngOnInit(): void {
+  findRepo () {
+    this.GetService.UpdateRepo(this.repoName);
+    this.GetService.searchrepos().subscribe(repo => {
+     
+      this.repoitems = repo["items"];
+      console.log(this.repoitems);
+    })
+  }
+
+  ngOnInit() {
+    this.findRepo()
   }
 
 }
