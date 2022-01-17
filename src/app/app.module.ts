2d1ccdb9ecd94cfe9940f06a596e1bfc1f5905fe
Lesley159 <lesleyhope159@gmail.com>
2022-01-17 03:38:15 +0300

    added httpclient module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -23,6 +24,9 @@ import { ErrorpageComponent } from './errorpage/errorpage.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    FormsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
